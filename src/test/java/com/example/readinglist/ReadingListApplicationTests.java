403865f718059db9e9b71eca91991eb9babd1378
dukevon <dukevon@163.com>
2016-08-25 16:17:32 +0800

    spring boot in action:chapter2-20160825

--- src/test/java/com/example/readinglist/ReadingListApplicationTests.java
+++ src/test/java/com/example/readinglist/ReadingListApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import readinglist.ReadingListApplication;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ReadingListApplication.class)
 @WebAppConfiguration
